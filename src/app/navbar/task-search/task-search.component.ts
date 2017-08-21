f852a16ef249b3aa58293d031b6c690288d3d347
Wagner Almeida <wdmeida@gmail.com>
2017-08-21 09:22:08 -0300

    Adding the task search component logic

--- src/app/navbar/task-search/task-search.component.ts
+++ src/app/navbar/task-search/task-search.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import "rxjs/add/observable/of";
+import "rxjs/add/operator/switchMap";
 
 import { Task } from './../../tasks/shared/task.model';
 import { TaskService } from './../../tasks/shared/task.service';
@@ -8,7 +14,27 @@ import { TaskService } from './../../tasks/shared/task.service';
   templateUrl: './task-search.component.html'
 })
 
-export class TaskSearchComponent {
+export class TaskSearchComponent implements OnInit {
+  public searchTerms: Subject<string> = new Subject();
+  public tasks: Task[] = [];
+  
+  public constructor(
+    private taskService: TaskService,
+    private router: Router
+  ) {}
 
-  public constructor(private taskService: TaskService) {}
+  public ngOnInit() {
+    this.searchTerms.switchMap(
+      term => term ? this.taskService.searchByTitle(term) : Observable.of<Task[]>([])
+    ).subscribe(tasks => this.tasks = tasks)
+  }
+
+  public search(term: string) {
+    this.searchTerms.next(term);
+  }
+
+  public gotoTask(task: Task) {
+    this.tasks = [];
+    this.router.navigate(['/tasks', task.id]);
+  }
 }
--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -61,6 +61,14 @@ export class TaskService {
               .map(() => null);
   }
 
+  public searchByTitle(term: string): Observable<Task[]> {
+    let url = `${this.tasksUrl}?title=${term}`;
+
+    return this.http.get(url)
+            .catch(this.handleErrors)
+            .map((response: Response) => response.json().data as Task[]);
+  }
+
   private handleErrors(error: Response) {
     console.log("SALVANDO O ERRO EM UM ARQUIVO DE LOG - DETALHES DO ERRO => ", error);
     return Observable.throw(error);
