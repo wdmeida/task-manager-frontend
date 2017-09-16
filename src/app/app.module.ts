e70cd8fc502b6ed01e1432f73b9b8130564ac16f
Wagner Almeida <wdmeida@gmail.com>
2017-09-16 16:37:43 -0300

    Adding jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,9 @@ import "rxjs/add/operator/switchMap";
 import "rxjs/add/observable/of";
 import "rxjs/add/observable/throw";
 
+// jquery plugins
+import * as $ from 'jquery';
+
 @NgModule({
   declarations: [
     AppComponent,
