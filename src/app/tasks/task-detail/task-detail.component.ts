5b3e2bfe6b2e2a159ea827ac7654698e6b70b8db
Wagner Almeida <wdmeida@gmail.com>
2017-09-18 19:27:19 -0300

    Adding group example

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -29,7 +29,11 @@ export class TaskDetailComponent implements OnInit, AfterViewInit {
       title: [null],
       deadline: [null],
       done: [null],
-      description: [null]
+      description: [null],
+      user: this.formBuilder.group({
+        name: ["João Carlos"],
+        email: ["joao@carlos.com"]
+      })
     });
   }
 
