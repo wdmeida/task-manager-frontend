58212cfa7b9c316e677a81e2c1867d862431a46c
Wagner Almeida <wdmeida@gmail.com>
2017-10-03 09:21:07 -0300

    Adding SignInFormComponent to AppModule and AppRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
 import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
 import { TasksComponent } from './tasks/tasks.component';
 
 const ROUTES = RouterModule.forRoot([
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'sign-in', component: SignInFormComponent },
   { path: 'sign-up', component: SignUpFormComponent },
   { path: 'tasks', component: TasksComponent },
   { path: 'tasks/:id', component: TaskDetailComponent },
