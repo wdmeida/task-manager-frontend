7e49028d28b7bc66c49b493225002712fda5a36b
Wagner Almeida <wdmeida@gmail.com>
2017-11-01 15:52:21 -0200

    Improving SignUp

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
+import { AuthService } from '../shared/auth.service';
 import { FormUtils } from './../shared/form.utils';
+import { User } from '../shared/user.model';
 
 @Component({
   selector: 'sign-up-form',
@@ -13,29 +16,39 @@ export class SignUpFormComponent {
   public formUtils: FormUtils;
 
   public constructor(
-    private formBuilder: FormBuilder
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
   ) {
+    this.setupForm();
+
+    this.formUtils = new FormUtils(this.form);
+  }
+
+  public signUpUser() {
+    this.authService.signUp(this.form.value as User)
+      .subscribe(
+        () => {
+          alert('Parabéns, sua conta foi criada com sucesso!');
+          this.router.navigate(['/dashboard']);
+        }
+      );
+  }
+
+  public passwordConfirmationValidator(form: FormGroup) {
+    if (form.get('password').dirty && form.get('password').value === form.get('passwordConfirmation').value) {
+      form.get('passwordConfirmation').setErrors(null);
+    } else {
+      form.get('passwordConfirmation').setErrors({ mismatch: true });
+    }
+  }
+
+  private setupForm() {
     this.form = this.formBuilder.group({
       name: [null, [ Validators.required, Validators.minLength(5), Validators.maxLength(100) ] ],
       email: [null, [ Validators.required, Validators.email ] ],
       password: [null, [ Validators.required, Validators.minLength(8) ] ],
       passwordConfirmation: [null, [ Validators.required ] ]
     }, { validator: this.passwordConfirmationValidator });
-
-    this.formUtils = new FormUtils(this.form);
-  }
-
-  public signUpUser() {
-    console.log('====================================');
-    console.log("Formulário de SignUp enviado");
-    console.log(this.form.value);
-    console.log('====================================');
-  }
-
-  public passwordConfirmationValidator(form: FormGroup) {
-    if (form.get('password').dirty && form.get('password').value === form.get('passwordConfirmation').value)
-      form.get('passwordConfirmation').setErrors(null);
-    else
-      form.get('passwordConfirmation').setErrors({ mismatch: true });
   }
 }
